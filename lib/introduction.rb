d39310dc47153c7aa34243322b1e89a463a6d1bd
looping-messaging-1494 <wilkins.jarred@outlook.com>
2019-07-14 20:53:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ end
 second_name= "Dan"
 language= "Ember"
 def introduction_with_language(name,language)
-  puts"Hi, my name is #{second_name} and I am learning to program in #{language}.\n"
+  puts "Hi, my name is #{second_name} and I am learning to program in #{language}.\n"
 end
